7965442e1c4b0aeaa92df6467a2ff05314e02a72
Cheonma <satyamupadhyay10525@gmail.com>
2026-06-13 17:41:14 +0530

    Day 5: Added JWT validation and email extraction

--- src/main/java/com/satyam/expensetracker/security/JwtService.java
+++ src/main/java/com/satyam/expensetracker/security/JwtService.java
@@ -8,12 +8,31 @@ import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY =
             "mySecretKeyForExpenseTrackerProject123456789mySecretKey";
+    public String extractEmail(String token) {
 
+        SecretKey key = Keys.hmacShaKeyFor(
+                SECRET_KEY.getBytes(StandardCharsets.UTF_8)
+        );
+
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
     public String generateToken(String email) {
 
         SecretKey key = Keys.hmacShaKeyFor(
